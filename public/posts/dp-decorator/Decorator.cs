b979fbfb025fb933b8d5a67e5b62005cf0b5a7cc
Linar Khilazhev <zzlinarzz@gmail.com>
2025-01-22 16:11:06 +0700

    Created Decorator.cs file

--- public/posts/dp-decorator/Decorator.cs
+++ public/posts/dp-decorator/Decorator.cs
@@ -1,31 +1,3 @@
-+++
-date = '2025-01-21T20:24:56+07:00'
-draft = false
-title = 'Design Patterns: Decorator'
-hideSummary = true
-+++
-
-# General
-
-Decorator. Hoorrayyy, it's first article in Design Patterns.
-
-I want to describe patterns, use cases, and examples, and nuances.
-
-# Decorator
-
-It's structural patterns, it's mean, xdd.
-
-## Shortly:
-1. Abstract class / Wrapper for logic
-2. Adding behaviour for base component.
-
-
-So, that how it looks it UML:
-
-![](decorator.drawio.svg)
-
-```csharp
-
 namespace DefaultNamespace;
 
 public class Main
@@ -93,7 +65,3 @@ public class SoundWebPageDecorator : BaseWebPageDecorator
         _currentWebPage = webPage;
     }
 }
-
-```
-
-Sounds, that how it looks!
